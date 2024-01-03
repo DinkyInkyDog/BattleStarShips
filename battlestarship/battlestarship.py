235280a031b27b7acbd00d95cf09ef091aeb7fe1
DinkyInkyDog <142037259+DinkyInkyDog@users.noreply.github.com>
2024-01-02 21:55:39 -0500

    create the grid

--- battlestarship/battlestarship.py
+++ battlestarship/battlestarship.py
@@ -1,10 +1,27 @@
-player_board = {"a": [1, 2, 3, 4, 5, 6, 7, 8, 9, 10], "b": [1, 2, 3, 4, 5, 6, 7, 8, 9, 10], "c": [1, 2, 3, 4, 5, 6, 7, 8, 9, 10], "d": [1, 2, 3, 4, 5, 6, 7, 8, 9, 10], "e": [1, 2, 3, 4, 5, 6, 7, 8, 9, 10], "f": [1, 2, 3, 4, 5, 6, 7, 8, 9, 10], "g": [1, 2, 3, 4, 5, 6, 7, 8, 9, 10], "h": [1, 2, 3, 4, 5, 6, 7, 8, 9, 10], "i": [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]}
+
+#I don't need this now But I might later.
+#player_board = {"a": [1, 2, 3, 4, 5, 6, 7, 8, 9, 10], "b": [1, 2, 3, 4, 5, 6, 7, 8, 9, 10], "c": [1, 2, 3, 4, 5, 6, 7, 8, 9, 10], "d": [1, 2, 3, 4, 5, 6, 7, 8, 9, 10], "e": [1, 2, 3, 4, 5, 6, 7, 8, 9, 10], "f": [1, 2, 3, 4, 5, 6, 7, 8, 9, 10], "g": [1, 2, 3, 4, 5, 6, 7, 8, 9, 10], "h": [1, 2, 3, 4, 5, 6, 7, 8, 9, 10], "i": [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]}
+
+
 letters = "abcdefghi"
 
 #there will be two boards. One for the player and one for the enemy that print seperate and hold their own info
 class Board:
-    def __init__(self):
-        fleet = []
+    top = """_________________________________________________________
+    |    1    2    3    4    5    6    7    8    9    10    |"""
+    row_a = "| A  .    .    .    .    .    .    .    .    .     .    |"
+    row_b = "| B  .    .    .    .    .    .    .    .    .     .    |"
+    row_c = "| C  .    .    .    .    .    .    .    .    .     .    |"
+    row_d = "| D  .    .    .    .    .    .    .    .    .     .    |"
+    row_e = "| E  .    .    .    .    .    .    .    .    .     .    |"
+    row_f = "| F  .    .    .    .    .    .    .    .    .     .    |"
+    row_g = "| G  .    .    .    .    .    .    .    .    .     .    |"
+    row_h = "| H  .    .    .    .    .    .    .    .    .     .    |"
+    row_i = "| I  .    .    .    .    .    .    .    .    .     .    |"
+    bottom = "_________________________________________________________"
+    def __init__(self, enemy = False):
+        self.fleet = []
+        self.enemy = enemy
     def generate_board(self):
         row_a = "| A"
         #I need to have i go through the fleet's locations and know if any of them are in the spaces
@@ -49,7 +66,7 @@ class Ship:
                 index += 1
 
 testship_1 = Ship(3)
-testship_1.flip()
+#testship_1.flip()
 
 testship_1.assign_ship("a", 4)
 print(testship_1.location)
